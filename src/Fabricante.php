8afca357e2cc001e3016df66ef90763ea0393f2f
Eliel Moura <eliellpf@gmail.com>
2023-10-03 11:40:54 -0300

    alteraçoes

---
+++ src/Fabricante.php
@@ -0,0 +1,55 @@
+<?php
+namespace ExemploCrudPoo;
+ final class Fabricante{
+
+    private int $id;
+    private string $nome;
+
+    /**
+     * Get the value of id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param int $id
+     *
+     * @return self
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     *
+     * @return string
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @param string $nome
+     *
+     * @return self
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+}
