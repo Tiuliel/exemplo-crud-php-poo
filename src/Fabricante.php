f0ba3d6f24035a85d3d897561ff52e1a9b1be2f4
Eliel Moura <eliellpf@gmail.com>
2023-10-05 09:20:52 -0300

    metodo de um fabricante

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -46,6 +46,20 @@ use PDO;
     
     }
 
+    public function lerUmFabricante():array {
+        $sql = "SELECT * FROM fabricantes WHERE id = :id";
+    
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao carregar: ".$erro->getMessage());
+        }
+    
+        return $resultado;
+    } 
 
 
 
