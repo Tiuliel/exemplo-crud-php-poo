45f763a166539e92629529866c899dba721d1b64
Eliel Moura <eliellpf@gmail.com>
2023-10-05 09:41:28 -0300

    refatorando atualizar

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -61,6 +61,18 @@ use PDO;
         return $resultado;
     } 
 
+    public function atualizarFabricante():void {
+        $sql = "UPDATE fabricantes SET nome = :nome WHERE id = :id";
+        
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao atualizar: ".$erro->getMessage());
+        }
+    } 
 
 
 
@@ -73,7 +85,7 @@ use PDO;
     
     public function setId(int $id): self
     {
-        $this->id = $id;
+        $this->id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
 
         return $this;
     }
