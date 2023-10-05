d3a823f19a4764354c37013d7fc2449da071f0a1
Eliel Moura <eliellpf@gmail.com>
2023-10-05 09:47:49 -0300

    refatorando funcçao fabricante

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -74,6 +74,16 @@ use PDO;
         }
     } 
 
+    public function excluirFabricante():void {
+        $sql = "DELETE FROM fabricantes WHERE id = :id";
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao excluir: ".$erro->getMessage());
+        }
+    }
 
 
     
